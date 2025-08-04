b305cb755ae8d50204fa63f77bde8dbbae07c9dc
Omkar Wazulkar <omkarwazulkar@Omkars-MacBook-Air.local>
2025-08-04 14:49:56 +0100

    Update

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000", 
-        "http://51.21.168.72:8000",
+        "https://51.21.168.72:8000/",
         "https://travel-planner-app-chi.vercel.app"
     ],
     allow_credentials=True,
@@ -17,4 +17,4 @@ app.add_middleware(
 
 @app.get("/")
 def read_root():
-   return {"message":"Hey Man, Omkar here"}
+   return {"message":"Hey Man, Omkar"}
