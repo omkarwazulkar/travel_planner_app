8090c065397caebf574b23daf520ddce274df6a6
Omkar Wazulkar <omkarwazulkar@Omkars-MacBook-Air.local>
2025-08-10 00:59:02 +0100

    Update

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://13.50.201.174"
+        "https://omkarwazulkar.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
